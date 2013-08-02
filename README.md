e93f5cc683daad10cff28635cd5fef6cc7e02008
Gedaly Guberek <gedalyguberek@gmail.com>
2013-08-01 20:46:29 -0700

    Deleted Updater, Simplified Structure

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 AWG-Client Plugin
 =========================
+Contributors: gedaly
 
+For Awesome Web Guy Clients
 
 WordPress Plugin for clients adding branding and links
 
@@ -9,3 +11,43 @@ WordPress Plugin for clients adding branding and links
 Add /awg-client directory to Plugins folder via FTP
 OR
 make .zip of /awg-client folder and upload via wordpress plugin uploader
+
+
+== Description ==
+
+Functions:
+-
+-
+
+== Changelog ==
+
+0.3.2
+deleted updater
+
+0.3
+Added Github updater
+Delete TODO file
+
+0.2.1
+Added Google campaign parameters to links
+Fixed menu links
+
+= 0.2 =
+* Added WP Lifeguard Videos
+
+= 0.1 =
+* First Commit
+* Custom Admin Footer 
+* Added Dropdown menu to Admin Bar
+
+
+=== FUNCTION REFERENCE ===
+
+== Admin Footer ==
+http://wordpress.org/support/topic/how-to-delete-change-admin-footer-text?replies=7
+
+== ADMIN BAR Menu Items ==
+http://wplifeguard.com/how-to-add-links-to-wordpress-3-3s-admin-bar/
+
+WP Lifeguard Videos
+http://wplifeguard.com/lifeguard-plugin/lifeguard-assistant-for-developers/ 
---
+++ awg-client.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+Plugin Name: awg-client
+Plugin URI: http://awesomewebguy.com/awg-client-plugin/?utm_source=wp-awg-plugin&utm_medium=link&utm_campaign=plugin-uri
+Description: Adding helpful links for Awesome Web Guy design and support clients.
+Version: 0.3.2
+Author: Gedaly Guberek
+Author URI: http://gedaly.com/?utm_source=wp-awg-plugin&utm_medium=link&utm_campaign=author-uri
+*/
+
+
+// Add custom footer in admin section
+
+function awg_custom_footer_admin () {
+    echo 'Built by <a href="http://awesomewebguy.com/?utm_source=wp-awg-plugin&utm_medium=link&utm_campaign=footer-link" target="_blank">Awesome Web Guy</a> | <a href="mailto:gedaly@awesomewebguy.com">Email Support</a></p>';
+    }
+
+    add_filter('admin_footer_text', 'awg_custom_footer_admin');
+
+
+
+// Add Dropdown Menu to WordPress Admin Bar
+
+add_action('admin_bar_menu', 'awg_add_toolbar_items', 100);
+function awg_add_toolbar_items($admin_bar){
+	$admin_bar->add_menu( array(
+		'id'    => 'awg-menu',
+		'title' => 'Awesome Web Guy',
+		'href'  => 'http://awesomewebguy.com/?utm_source=wp-awg-plugin&utm_medium=link&utm_campaign=wp-admin-bar-link',	
+		'meta'  => array(
+			'title' => __('Awesome Web Guy'),	
+			'target' => '_blank',		
+		),
+	));
+	$admin_bar->add_menu( array(
+		'id'    => 'awg-support',
+		'parent' => 'awg-menu',
+		'title' => 'Support',
+		'href'  => 'http://awesomewebguy.com/contact/?utm_source=wp-awg-plugin&utm_medium=link&utm_campaign=wp-admin-bar-link',
+		'meta'  => array(
+			'title' => __('Support'),
+			'target' => '_blank',
+			'class' => 'awg_menu_item_class'
+		),
+	));
+	$admin_bar->add_menu( array(
+		'id'    => 'awg-wp-tutorials',
+		'parent' => 'awg-menu',
+		'title' => 'Blog & Tutorials',
+		'href'  => 'http://awesomewebguy.com/blog/?utm_source=wp-awg-plugin&utm_medium=link&utm_campaign=wp-admin-bar-link',
+		'meta'  => array(
+			'title' => __('Blog & Tutorials'),
+			'target' => '_blank',
+			'class' => 'awg_menu_item_class'
+		),
+	));
+}
+
+
+// Add WP Lifeguard Tutorial Video Section
+
+add_action('admin_menu', 'lgap_add_pages');
+
+
+function lgap_add_pages()  {
+add_menu_page(__('Help Videos','menu-test'), __('Help Videos','menu-test'), 'read', 'lifeguard-assistant-plugin', 'lgap_main_page' );
+}
+
+function lgap_main_page() {echo "<h2>" . __( 'Help Videos', 'menu-test' ) . "</h2>";
+
+
+     echo '<style type="text/css">#wplg { font-family: "Varela",Helvetica,Trebuchet MS,Verdana,"DejaVu Sans",sans-serif; }#wplg a:link,#wplg a:visited { color: #21759b; text-decoration: none; }#wplg a:hover { color: #d54e21; }.wplg-video { background: #f6f6f6; border: 1px solid #dadada; padding: 12px; margin: 0 12px 12px 0; float: left; }.wplg-clear { clear: both; }.wplg-green-button { box-shadow:inset 0 0 3px rgba(0,0,0,.1); font-size: 20px; line-height: 32px; height: 32px; width: 434px; margin: 0 12px 12px 0; text-align: center; display: block; border: 2px solid #9abf89; background: #7da742; color: #f1ffeb !important; text-shadow: 0 0 3px rgba(125,167,66,.75); }.wplg-green-button:hover { border: 2px solid #c0e1aa; background: #8ac636; }.wplg-green-button:active { border: 2px solid #88a65e; background: #5d822a; }</style><link href="http://fonts.googleapis.com/css?family=Varela" rel="stylesheet" type="text/css"><div id="wplg"><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32852753?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32856785?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32857648?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32860297?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32872861?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32878118?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32881530?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32864178?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32863614?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32862744?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-video"><iframe src="http://player.vimeo.com/video/32857481?title=0&byline=0&portrait=0&color=ffffff" width="412" height="309" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div><div class="wplg-clear"></div></div>';}         
+
+ 
+
+
+?>
